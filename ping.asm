ed82087e140554fd88dfd8564f1fec5ad94e2212
SenseNet <unluck.bastard@gmail.com>
2026-08-09 22:58:29 +0300

    think about ping util

--- ping.asm
+++ ping.asm
@@ -5,7 +5,7 @@
 global _start
 
 section .data
-    ERROR_MSG db "PING ERROR"
+    ERROR_MSG db "PING ERROR", 0xA
     ERROR_MSG_LEN equ ($ - ERROR_MSG)
 
 section .rodata
@@ -16,15 +16,33 @@ section .bss
 section .text
 _start:
 ;GET STRING FOR IP ADDRES
-    mov rsi, READER_BUFFER
-    mov rdx, 15
-    call _READ
+    mov rbx, [rsp]
+;More than 3 arg (name, addr, -t opt)
+    cmp rbx, 3
+    jg .errorexit
+;Less than 2arg (name, addr)    
+    cmp rbx, 1
+    jle .errorexit
 
-    mov rsi, READER_BUFFER
+    lea r12, [rsp + 8]
+
+    mov rsi, [rsp + 16]
+    call _STRLEN
+
+    mov rsi, [rsp + 16]
     mov rdx, rax
     call _PRINT_STR
 
-.noerror:
+.noerrorexit:
     mov rax, 60
     mov rdi, 0
+    syscall
+
+.errorexit:
+    mov rsi, ERROR_MSG
+    mov rdx, ERROR_MSG_LEN
+    call _PRINT_STR
+
+    mov rax, 60
+    mov rdi, 1
     syscall

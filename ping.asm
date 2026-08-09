4b6f41af4ba19eb88c6176fade6a573c6887c4b2
SenseNet <unluck.bastard@gmail.com>
2026-08-09 23:53:54 +0300

    think about ping util

--- ping.asm
+++ ping.asm
@@ -8,10 +8,23 @@ section .data
     ERROR_MSG db "PING ERROR", 0xA
     ERROR_MSG_LEN equ ($ - ERROR_MSG)
 
+struc SOCKADDR_IN
+    .sin_family: resw 1 ;word 2 bytes (AF_INET = 2)
+    .sin_port:   resw 1 ;word 2 bytes (Port in network byte order)
+    .sin_addr:   resd 1 ;double word 4 bytes (IP address / INADDR_ANY)
+    .sin_zero:   resb 8 ;bytes 8 bytes (Padding to match sockaddr size)
+endstruc
+
+SOCKADDR_SIZE equ 16
+
+
 section .rodata
+    TIME_KEY db "-t"
+
 
 section .bss
     READER_BUFFER resb 15
+
 
 section .text
 _start:
@@ -32,7 +45,7 @@ _start:
     mov rsi, [rsp + 16]
     mov rdx, rax
     call _PRINT_STR
-
+    
 .noerrorexit:
     mov rax, 60
     mov rdi, 0
